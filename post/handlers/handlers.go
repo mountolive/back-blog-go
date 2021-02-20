53f76fa1790e29b1a9fd9d7936ca87b120883f77
Leo Guercio <lpguercio@gmail.com>
2021-02-20 14:29:39 +0100

    Change Params to have a Validate method

--- post/handlers/handlers.go
+++ post/handlers/handlers.go
@@ -6,23 +6,49 @@ import (
 	"github.com/mountolive/back-blog-go/post/usecase"
 )
 
-// Query is simply the name of the query, for lookup purposes
-type Query string
-
-// Command is simply the name of the command, for lookup purposes
-type Command string
+// Params describes a basic query or command
+type Params interface {
+	Validate() error
+}
 
 // CommandHandler defines the basic methods for a Command in the system
 type CommandHandler interface {
-	Handle(context.Context, Command) (usecase.PostDto, error)
+	Handle(context.Context, Params) (*usecase.PostDto, error)
 }
 
 // QueryOneHandler defines the basic methods for a Query of one element in the system
 type QueryOneHandler interface {
-	Handle(context.Context, Query) (usecase.PostDto, error)
+	Handle(context.Context, Params) (*usecase.PostDto, error)
 }
 
 // QueryManyHandler defines the basic methods for a Query of many elements in the system
 type QueryManyHandler interface {
-	Handle(context.Context, Query) ([]usecase.PostDto, error)
+	Handle(context.Context, Params) ([]*usecase.PostDto, error)
+}
+
+// CreatePostCommandHandler is self-described
+type CreatePostCommandHandler struct {
+	repo usecase.PostRepository
+}
+
+// UpdatePostCommandHandler is self-described
+type UpdatePostCommandHandler struct {
+	repo usecase.PostRepository
+}
+
+var (
+	_ CommandHandler = CreatePostCommandHandler{}
+	_ CommandHandler = UpdatePostCommandHandler{}
+)
+
+// Handle has the requirements to create a Post, according to the Params passed
+func (c CreatePostCommandHandler) Handle(ctx context.Context, p Params) (*usecase.PostDto, error) {
+	// TODO Implement
+	return nil, nil
+}
+
+// Handle has the requirements to update a Post, according to the Params passed
+func (u UpdatePostCommandHandler) Handle(ctx context.Context, p Params) (*usecase.PostDto, error) {
+	// TODO Implement
+	return nil, nil
 }
---
+++ post/handlers/handlers_test.go
@@ -0,0 +1,13 @@
+package handlers_test
+
+import "testing"
+
+func TestCreatePostCommandHandler(t *testing.T) {
+	t.Run("Handle", func(t *testing.T) {
+	})
+}
+
+func TestUpdatePostCommandHandler(t *testing.T) {
+	t.Run("Handle", func(t *testing.T) {
+	})
+}
