b72c2db38beb2b48bd1e3bdd9006d9eedab25144
leo guercio <lpguercio@gmail.com>
2021-12-11 11:52:30 +0100

    Add meta tags

--- front/app.tsx
+++ front/app.tsx
@@ -5,8 +5,10 @@ export default function App({ Page, pageProps }: { Page: FC, pageProps: Record<s
     <main>
       <head>
         <meta name="viewport" content="width=device-width" />
-        <meta property="og:title" content="Leo Guercio's blog" />
-        <meta property="og:image" content="https://www.aparences.net/wp-content/uploads/2017/11/de-chirico-portrait-apollinaire.jpg" />
+        <meta name="title" property="og:title" content="Leo Guercio's blog" />
+        <meta name="type" property="og:type" content="website" />
+        <meta name="image" property="og:image" content="https://www.aparences.net/wp-content/uploads/2017/11/de-chirico-portrait-apollinaire.jpg" />
+        <meta name="url" property="og:url" content="https://www.leoponc.io" />
       </head>
       <Page {...pageProps} />
     </main>
